80bc163351b9476ac743da5b61e1a9b90bdbb019
Bruno Campos <campos.bruno98@gmail.com>
2023-10-18 19:33:17 -0300

    feat: retrieve all online contacts and show them in the chat page

--- web/src/app/chat/page.tsx
+++ web/src/app/chat/page.tsx
@@ -2,8 +2,14 @@
 import { useEffect, useState } from 'react'
 import { AiOutlineSend } from 'react-icons/ai'
 
+interface User {
+  userId: string
+  username: string
+}
+
 export default function Chat() {
   const [ws, setWs] = useState<null | WebSocket>(null)
+  const [onlinePeople, setOnlinePeople] = useState<any>({})
 
   useEffect(() => {
     const ws = new WebSocket('ws://localhost:3030')
@@ -11,42 +17,69 @@ export default function Chat() {
     ws.addEventListener('message', handleMessage)
   }, [])
 
+  function showOnlinePeople(peopleArray: User[]) {
+    const people: any = {}
+    peopleArray.forEach(({ userId, username }) => {
+      people[userId] = username
+    })
+    setOnlinePeople(people)
+  }
+
   function handleMessage(e: any) {
-    console.log('new message: ', e)
+    const messageData = JSON.parse(e.data)
+    if (messageData.online) {
+      showOnlinePeople(messageData.online)
+    }
   }
 
   return (
-    <div className="flex">
-      <section className="w-1/4 h-[100vh] border-r border-black bg-slate-600 text-white">
-        <div className="flex justify-around border-black h-16 items-center border border-r-0">
-          <div className="flex-1">
-            <p className="flex-1 text-center border-black font-bold text-xl hover:cursor-pointer">Groups</p>
+    <div className="bg-emerald-100">
+      <div className="flex w-[1660px] mx-auto border-black border">
+        <section className="w-1/4 h-[100vh] border-r border-black bg-slate-700 text-white overflow-hidden">
+          <div className="h-16 border-black border-b">
+            <h1 className="h-full flex items-center text-4xl font-bold justify-center pb-2 bg-slate-900">Pappo</h1>
           </div>
-          <p className="h-full border border-black"></p>
-          <div className="flex-1 bg-slate-500 h-full flex justify-center items-center">
-            <p className=" text-center border-black font-bold text-xl hover:cursor-pointer">Contacts</p>
+          <div className="flex justify-around border-black h-16 items-center border-b">
+            <div className="flex-1">
+              <p className="flex-1 text-center border-black font-bold text-xl hover:cursor-pointer">Groups</p>
+            </div>
+            <p className="h-full border border-black"></p>
+            <div className="flex-1 bg-slate-500 h-full flex justify-center items-center">
+              <p className=" text-center border-black font-bold text-xl hover:cursor-pointer">Contacts</p>
+            </div>
           </div>
-        </div>
-      </section>
-      <section className="w-3/4 h-[100vh] flex flex-col">
-        <div className="h-16 border border-l-0 border-black pl-8 bg-slate-600 text-white flex items-center">
-          <p className="w-fit text-2xl font-bold">Contact name</p>
-        </div>
-        <div className="flex-1 bg-emerald-200"></div>
-        <div className="flex bg-emerald-400 h-14 items-center justify-around">
-          <div className="font-bold text-xl w-fit px-3 hover:cursor-pointer">Add File</div>
-          <form className="w-[90%] flex gap-4" action="">
-            <input
-              className="py-1.5 rounded-2xl w-full pl-3 text-lg focus:outline-none"
-              type="text"
-              placeholder="Digite sua mensagem"
-            />
-            <button className=" right-10 bottom-3.5 font-bold text-3xl text-white bg-emerald-500 px-2">
-              <AiOutlineSend />
-            </button>
-          </form>
-        </div>
-      </section>
+          {Object.keys(onlinePeople).map((userId: any) => {
+            return (
+              <div className="w-full bg-slate-700 h-14  border-b border-black flex gap-3 pl-2" key={userId}>
+                <section>Photo</section>
+                <section className="h-full w-full pl-2">
+                  <p className="flex flex-col justify-center text-2xl font-bold items-start">{onlinePeople[userId]}</p>
+                  <p>Last message</p>
+                </section>
+              </div>
+            )
+          })}
+        </section>
+        <section className="w-full h-[100vh] flex flex-col">
+          <div className="h-16 border-b border-black pl-8 bg-slate-700 text-white flex items-center">
+            <p className="w-fit text-2xl font-bold">Contact name</p>
+          </div>
+          <div className="flex-1 bg-emerald-200"></div>
+          <div className="flex bg-emerald-400 h-14 items-center justify-around">
+            <div className="font-bold text-xl w-fit px-3 hover:cursor-pointer">Add File</div>
+            <form className="w-[90%] flex gap-4" action="">
+              <input
+                className="py-1.5 rounded-2xl w-full pl-3 text-lg focus:outline-none"
+                type="text"
+                placeholder="Digite sua mensagem"
+              />
+              <button className=" right-10 bottom-3.5 font-bold text-3xl text-white bg-emerald-500 px-2">
+                <AiOutlineSend />
+              </button>
+            </form>
+          </div>
+        </section>
+      </div>
     </div>
   )
 }
